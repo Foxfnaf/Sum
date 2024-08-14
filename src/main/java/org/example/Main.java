90fa9b42c746ec8c71a85e31a2dd8b7fb2a37045
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-14 16:24:56 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] values = {1};
+        int startValue = 0;
+
+        getSum(new int[]{1, 2, 3}, 1);
+    }
+    public static int getSum(int[] values, int startValue) {
+        int sum = 0;
+        if (Arrays.stream(values).anyMatch(value -> value == startValue)) {
+            for (int i = 0; i < values.length; i++) {
+               if(values[i] > startValue) {
+                   sum += values[i];
+               }
+            }
+        }else sum = 0;
+        System.out.println(sum);
+
+        return sum;
     }
 }

301c74590dae880385f04132f70c422f399b1c0f
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-16 14:34:28 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,34 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.OptionalInt;
 
 public class Main {
     public static void main(String[] args) {
-        int[] values = {1};
-        int startValue = 0;
-
-        getSum(new int[]{1, 2, 3}, 1);
+        getSum(new int[] {10}, 10);
     }
     public static int getSum(int[] values, int startValue) {
         int sum = 0;
-        if (Arrays.stream(values).anyMatch(value -> value == startValue)) {
-            for (int i = 0; i < values.length; i++) {
-               if(values[i] > startValue) {
-                   sum += values[i];
-               }
-            }
-        }else sum = 0;
-        System.out.println(sum);
+        int index = -1;
+        OptionalInt foundNumber = Arrays.stream(values)
+                .filter(value -> value == startValue)
+                .findFirst();
 
+        try{
+           index = Arrays.stream(values).boxed().toList().indexOf(foundNumber.getAsInt());
+        }catch (NoSuchElementException e){
+            sum = 0;
+            System.out.println(sum);
+            return sum;
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            if(i > index){
+                sum+= values[i];
+            }else sum = 0;
+        }
+        System.out.println(sum);
         return sum;
     }
 }

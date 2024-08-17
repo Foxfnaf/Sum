5e55bd63b63f86df2aee3ff622c1b6e7e9f5df9d
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-17 10:54:27 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        getSum(new int[] {1, 2, 3}, 1);
+        getSum(new int[]{1, 2, 3}, 1);
     }
 
-public static int getSum(int[] values, int startValue) {
-    boolean found = false;
-    int result = 0;
-    for (int value : values) {
-        if (found) result += value;
-        else if (value == startValue) found = true;
+    public static int getSum(int[] values, int startValue) {
+        boolean found = false;
+        int result = 0;
+        for (int value : values) {
+            if (found) {
+                result += value;
+            } else if (value == startValue) {
+                found = true;
+            }
+        }
+        System.out.println(result);
+
+        return result;
     }
-    System.out.println(result);
-    return result;
-}
 }
